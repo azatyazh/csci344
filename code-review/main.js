6b4879ea1f7b9f1487065facf03fa7a3d5da34eb
Alina <azatyazhnykh@gmail.com>
2025-01-24 16:03:48 -0500

    my first commit

---
+++ code-review/main.js
@@ -0,0 +1,59 @@
+console.log("Hello World!");
+
+// let age = prompt("How old are you?");
+
+// if (age > 19 && age < 30) {
+//     console.log("You are in your 20s");
+// }
+// else if(age > 29 && age < 40){
+//     console.log("You are in your 30s");
+// }
+// else if(age > 39 && age < 50){
+//     console.log("You are in your 40s");
+// }
+// else console.log("IDK");
+
+
+// // red, blue, and yellow 
+
+// let red = prompt("Would you like color red (yes or no)?");
+// let blue = prompt("Would you like color blue (yes or no)?");
+// let yellow = prompt("Would you like color yellow (yes or no)?");
+
+// console.log(red, blue, yellow);
+
+// if (red == "yes" && blue == "yes" && yellow == "yes") {
+//     console.log("black");
+// }
+// else if (red == "yes" && blue == "yes" && yellow == "no"){
+//     console.log("purple");
+// }
+// else if (red == "yes" && blue == "no" && yellow == "yes"){
+//     console.log("orange");
+// }
+// else if (red == "no" && blue == "yes" && yellow == "yes"){
+//     console.log("green");
+// }
+// else if (red == "yes" && blue == "no" && yellow == "no"){
+//     console.log("red");
+// }
+// else if (red == "" && blue == "" && yellow == "no"){
+//     console.log("blue");
+// }
+// else if (red == "no" && blue == "no" && yellow == "yes"){
+//     console.log("yellow");
+// }
+// else if (red == "" && blue == "" && yellow == "yes"){
+//     console.log("white");
+// }
+// else console.log("none");
+
+let i = 1; 
+
+while (i < 99) {
+    console.log(i++); 
+}
+
+
+
+
