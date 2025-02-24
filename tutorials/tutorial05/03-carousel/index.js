5b9b4999538ec51827b04478c0455a2cf52e5ae2
Alina <azatyazhnykh@gmail.com>
2025-02-24 12:57:32 -0500

    tutorial5

--- tutorials/tutorial05/03-carousel/index.js
+++ tutorials/tutorial05/03-carousel/index.js
@@ -2,24 +2,33 @@ let currentPosition = 0;
 let gap = 10;
 const slideWidth = 400;
 
-function moveCarousel(direction) {
-    const items = document.querySelectorAll(".carousel-item");
+function moveCarousel(direction) { //function that moves the carusel 
+    const items = document.querySelectorAll(".carousel-item"); // geting all the caruousel items
 
-    if (direction == "forward") {
+    if (direction == "forward") { //move forward 
         // minus 2 b/c first 2 slides already showing
         if (currentPosition >= items.length - 2) {
             return false;
         }
+
+        //checking if we are at the last possible position leaving 2 slides visible
+        //if yes, return false to prevent movement? 
         currentPosition++;
+        //increment 
     } else {
+        //move backwards 
+        //if at first position '0' we don't go further 
         if (currentPosition == 0) {
             return false;
         }
+        //subtract the position movement backwards 
         currentPosition--;
     }
 
+    //calculate depending on gap, width, and current position 
     const offset = (slideWidth + gap) * currentPosition;
 
+    //loop through 
     for (const item of items) {
         item.style.transform = `translateX(-${offset}px)`;
     }
