bc3463b12a80ff034e534b79e12fc3af9fb62467
Alina <azatyazhnykh@gmail.com>
2025-03-07 13:51:41 -0500

    quiz2

---
+++ quizzes/quiz02a/task01/main.js
@@ -0,0 +1,24 @@
+// your function here
+
+
+//  document.querySelector('#container').addEventListener('click',function(){
+//     document.body.classList.toggle('dark-mode');
+//  })
+
+ function darkMode() {
+    const dark = document.querySelector('#container');
+
+    dark-darkMode.classList.toggle("active");
+    Element.classList.addEventListener("click", darkMode);
+ }
+
+
+// function toggleMenu() {
+//     const nav = document.querySelector(".nav-links");
+//     const button = document.querySelector("#menu-toggle");
+
+//     nav.classList.toggle("active");
+//     button.classList.toggle("active");
+
+// }
+ 
---
+++ quizzes/quiz02a/task02/main.js
@@ -0,0 +1,9 @@
+// your function here
+
+const show = (ev) =>{
+    ev.preventDefault();
+
+   show = document.querySelector("#track-list");
+
+   console.log(search);
+}
